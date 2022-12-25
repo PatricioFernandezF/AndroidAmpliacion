0aeab3a57f8132e4790f1630c30f6d1fd194c248
patriciouca <patricio.fernandezflorez@alum.uca.es>
2022-12-25 18:43:03 +0100

    fragment_detalle

---
+++ app/src/main/java/com/example/myapplication/fragments/DetalleFragment.java
@@ -0,0 +1,54 @@
+package com.example.myapplication.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.Adaptadores.AdapterPeliculas;
+import com.example.myapplication.R;
+import com.example.myapplication.databinding.FragmentDetalleBinding;
+import com.example.myapplication.databinding.FragmentListadoBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DetalleFragment extends Fragment {
+
+    private FragmentDetalleBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentDetalleBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        binding.volver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(DetalleFragment.this)
+                        .navigate(R.id.action_DetalleFragment_to_ListadoFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
